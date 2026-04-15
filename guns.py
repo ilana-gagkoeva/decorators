c788ea396b1bfb38dacd7701d0866669f8cdcea1
Ilana Gagkoeva <gagkoevai.m@gmail.com>
2026-04-15 18:32:48 +0300

    Testing

--- guns.py
+++ guns.py
@@ -23,3 +23,77 @@ class Pistol(Gun):
         
     def shoot(self):
         super().shoot()
+
+class Automat(Gun):
+    """Автомат"""
+
+    @property
+    def title(self):
+        return 'Automat'
+
+
+    def shoot(self):
+        super().shoot()
+
+
+class MachineGun(Gun):
+    """Пулемёт"""
+
+    @property
+    def title(self):
+        return 'MachineGun'
+
+    def shoot(self):
+        super().shoot()
+
+class SilencerDecorator(Gun):
+    """Декоратор глушителя"""
+
+class LightingDecorator(Gun):
+    """Декоратор для фонарика"""
+
+class AllGunSilencerDecorator(SilencerDecorator):
+    @property
+    def title(self):
+        return self._gun.title
+        
+    def __init__(self, gun: Gun):
+        self._gun = gun   
+        
+    def shoot(self):
+        print('Надеваем глушитель...')
+        self._gun.shoot()
+        print('Снимаем глушитель...')
+        
+class PistolSilencerDecorator(SilencerDecorator):
+    @property
+    def title(self):
+        return self._pistol.title
+        
+    def __init__(self, pistol: Pistol):
+        self._pistol = pistol
+        
+    def shoot(self):
+        print('Надеваем специальный глушитель для пистолетов...')
+        self._pistol.shoot()
+        print('Снимаем специальный глушитель для пистолетов...')
+
+# создание пистолета, автомата, пулемёта
+p = Pistol()
+a = Automat()
+mg = MachineGun()
+
+# создание пистолета с глушителем
+sp = AllGunSilencerDecorator(p)
+# создание автомата с глушителем
+sa = AllGunSilencerDecorator(a)
+# создание пулемёта с глушителем
+smg = AllGunSilencerDecorator(mg)
+
+# создание пистолета со специальным глушителем для пистолетов
+ssp = PistolSilencerDecorator(p)
+
+# стрельба из оружия
+for gun in [p, a, mg, sp, sa, smg, ssp]:
+    gun.shoot()
+    
